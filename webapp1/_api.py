278b6c18b7fcef38fa4a36fc4607d4a3fc49fe33
luap42 / Paul <pstrobach15+luap42@gmail.com>
2020-12-21 19:46:14 +0100

    Allow sending and receiving messages

--- webapp1/_api.py
+++ webapp1/_api.py
@@ -1,27 +1,60 @@
+from flask import *
+import protocol.reference as sec
 import sys
+import os
+import uuid
 sys.path.append("..")
 
-import protocol.reference as sec
-from flask import *
 
 api = Blueprint("api", __name__)
+
 
 @api.route("/")
 def index():
     return "SEC API"
 
+
 @api.route("/certfile")
 def certfile():
     return "Certfile comes here"
+
 
 @api.route("/certfile/challenge")
 def certfile_challenge():
     return "Challenge comes here"
 
+
 @api.route("/user/<handle>/certfile")
 def user_certfile(handle):
-    return f"User Certfile for {handle} comes here"
+    if handle.isidentifier():
+        if os.path.isdir(os.path.abspath("./storage/users/" + handle + "/")):
+            f = open("storage/users/" + handle + "/cert/user.certfile", "r")
+            certfile = f.read()
+            f.close()
+            return certfile
+        else:
+            abort(404)
+    else:
+        abort(400)
+
 
 @api.route("/user/<handle>/recv", methods=["POST"])
 def user_recv(handle):
-    return "REJECTED\nThis server is not ready to receive messages"
+    try:
+        if handle.isidentifier():
+            if os.path.isdir(os.path.abspath("./storage/users/" + handle + "/")):
+                message_id = str(uuid.uuid4())
+                f = open("storage/messages/" + message_id, "w")
+                f.write(request.get_data().decode("utf-8"))
+                f.close()
+
+                f = open("storage/users/" + handle + "/inbox", "a")
+                f.write(message_id + " [noch nicht geöffnet]\n")
+                f.close()
+                return "OK\n" + message_id
+            else:
+                return "REJECTED\nUser doesn't exist"
+        else:
+                return "REJECTED\nUser Handle is invalid"
+    except Exception as e:
+        return "ERROR\n" + str(e)
--- webapp1/_ui.py
+++ webapp1/_ui.py
@@ -1,22 +1,64 @@
-import sys, os
+from flask import *
+import protocol.reference as sec
+from datetime import datetime
+import sys
+import os
+import urllib
 sys.path.append("..")
 
-import protocol.reference as sec
-from flask import *
 
 ui = Blueprint("ui", __name__)
+
 
 @ui.route("/")
 def index():
     return render_template("index.html")
 
+
 @ui.route("/inbox")
 def inbox():
     return render_template("inbox.html")
 
+
 @ui.route("/write")
 def write():
     return render_template("write.html")
+
+
+@ui.route("/write", methods=["POST"])
+def write_do():
+    to, subject, body = request.form["to"], request.form["subject"], request.form["body"]
+    to_handle, to_server = to.split("@")
+
+    user_cert = sec.CertFile.load(
+        "storage/users/" + session['user'] + "/cert/user.certfile")
+    user_cert = user_cert.cert()
+
+    with urllib.request.urlopen("http://" + to_server + "/api/user/" + to_handle + "/certfile") as cf:
+        cfc = cf.read().decode("utf-8")
+    recipient_cert = sec.CertFile.parse(cfc)
+    recipient_cert = recipient_cert.cert()
+
+    user_privkey_sign = sec.loadPrivateKey(
+        "storage/users/" + session["user"] + "/cert/user", "sign", passphrase=session["password"])
+
+    m = sec.Message(subject, "text/raw", body.encode("utf-8"),
+                    user_cert, recipient_cert, datetime.now())
+    m = m.encrypt(recipient_cert)
+    m_code = m.build_signed(user_privkey_sign)
+
+    req = urllib.request.Request(
+        "http://" + to_server + "/api/user/" + to_handle + "/recv")
+    req.method = "POST"
+    req.data = m_code.encode("utf-8")
+    try:
+        with urllib.request.urlopen(req) as res:
+            response = res.read().decode("utf-8")
+    except:
+        response = "ERROR\nServer-Fehler"
+
+    return render_template("write_done.html", response=response)
+
 
 @ui.route("/login", methods=["GET", "POST"])
 def login():
@@ -26,7 +68,8 @@ def login():
         if handle.isidentifier():
             if os.path.isdir(os.path.abspath("./storage/users/" + handle + "/")):
                 try:
-                    sec.loadPrivateKey("storage/users/"+handle+"/cert/user", "recv", passphrase=pw)
+                    sec.loadPrivateKey(
+                        "storage/users/"+handle+"/cert/user", "recv", passphrase=pw)
                 except:
                     error = "Dieses Benutzerkonto existiert nicht."
             else:
@@ -41,6 +84,7 @@ def login():
             return redirect(url_for("ui.index"))
 
     return render_template("login.html", error=error)
+
 
 @ui.route("/register", methods=["GET", "POST"])
 def register():
@@ -62,6 +106,7 @@ def register():
             return redirect(url_for("ui.index"))
     return render_template("register.html", error=error)
 
+
 @ui.route("/logout")
 def logout():
     session['in'] = False
@@ -77,12 +122,15 @@ def _generate_user_data(name, handle, pw):
     f = open("storage/service_cert/passphrase", "r")
     server_passphrase = f.read()
     f.close()
-    server_privkey_sign = sec.loadPrivateKey("storage/service_cert/service", "sign", passphrase=server_passphrase)
+    server_privkey_sign = sec.loadPrivateKey(
+        "storage/service_cert/service", "sign", passphrase=server_passphrase)
 
-    cert, privkey_sign, privkey_recv = sec.Certificate.newUser(name, handle + "@" + server_cert.Handle, [], server_cert)
+    cert, privkey_sign, privkey_recv = sec.Certificate.newUser(
+        name, handle + "@" + server_cert.Handle, [], server_cert)
 
     os.makedirs("storage/users/" + handle + "/cert")
 
-    sec.storeCert(cert, privkey_sign, privkey_recv, to="storage/users/" + handle + "/cert/user", sign=server_privkey_sign, passphrase=pw)
+    sec.storeCert(cert, privkey_sign, privkey_recv, to="storage/users/" +
+                  handle + "/cert/user", sign=server_privkey_sign, passphrase=pw)
 
     open("storage/users/" + handle + "/inbox", "w").close()
