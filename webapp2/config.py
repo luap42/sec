0f5cec6894e53d816ba13e1a81ce646a59208f70
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-01-09 16:55:15 +0100

    Fix linter

--- webapp2/config.py
+++ webapp2/config.py
@@ -3,11 +3,13 @@ SETTINGS = {
     #
     # DATABASE_URL
     # ------------
-    # This setting contains the information neccessary to connect to the MySQL server
-    # for this webapp2 application.
-    #   Uses the following format: 'mysql://[username]:[password]@[server]/[database]',
-    # where username and password are the credentials for a MySQL user and database and server
-    # identify the database you want the webapp2 data to be stored in.
+    # This setting contains the information neccessary to connect to the MySQL
+    # server for this webapp2 application.
+    #   Uses the following format:
+    #               'mysql://[username]:[password]@[server]/[database]',
+    # where username and password are the credentials for a MySQL user and
+    # database and server identify the database you want the webapp2 data to be
+    # stored in.
     #
     'DATABASE_URL': 'mysql+pymysql://sec:sec.local.db@localhost/sec_webapp2',
     #
